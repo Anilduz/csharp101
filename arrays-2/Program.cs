c9d9670ae6513b9f232e19677a418ed984d854ff
Anilduz <anilduz34@gmail.com>
2022-07-10 00:32:02 +0300

    array-2-push

---
+++ arrays-2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace arrays_2{
+    class Program {
+        public static void Main(string[] args){
+            Console.Clear();
+            Console.Clear();
+
+            //sort
+            int[] dizi1 = {23,12,4,86,72,3,11,17};
+
+            Console.WriteLine("*********** sırasız array");
+            foreach (var item1 in dizi1)
+            {
+                Console.WriteLine(item1);
+            }
+
+            Console.WriteLine("*********** sıralı array");
+            Array.Sort(dizi1);
+            foreach (var item in dizi1)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            //array clear
+            Console.WriteLine("*********** array clear");
+            Array.Clear(dizi1,2,2);
+            foreach (var item3 in dizi1)
+            {
+                Console.WriteLine(item3);
+            }
+
+            //reverse
+            Console.WriteLine("*********** array reverse");
+
+            Array.Reverse(dizi1);
+
+             foreach (var item4 in dizi1)
+            {
+                Console.WriteLine(item4);
+            }
+
+            //index of
+            Console.WriteLine("*********** array indexof");
+
+            int b = Array.IndexOf(dizi1,23);
+
+            Console.WriteLine(b);
+
+            //resize
+            Console.WriteLine("*********** array resize");
+            Array.Resize<int>(ref dizi1,15);
+
+            dizi1[8] = 99;
+            foreach (var item5 in dizi1)
+            {
+                Console.WriteLine(item5);
+            }
+
+
+        }
+    }
+}
