6662321e77d3cebaa527bf4ec47ef3e0edb89c0c
Anilduz <anilduz34@gmail.com>
2022-07-17 13:21:27 +0300

    stringLab

---
+++ Karakter-Tersten-Yazdirma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TerstenYazdir{
+    class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Kelime veya Cümleyi Tersten yazdıran programa hoşgeldiniz!");
+            Console.WriteLine("lütfen kelime veya cümle giriniz");
+            var text = Console.ReadLine().ToString();
+      
+            Islemler islem = new Islemler();
+            islem.Reverse(text);
+            Console.ReadKey();
+        }
+        
+    }
+    class Islemler{
+        public void Reverse(string str){
+            
+            if ((str == null) || (str.Length <= 1))
+            {
+            Console.Write(str); 
+            }
+            else
+            { 
+                Console.Write(str[str.Length-1]); 
+                Reverse(str.Substring(0,(str.Length-1))); 
+            } 
+        }
+    }
+}
