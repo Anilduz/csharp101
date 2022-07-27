45b29d5804ce371335a7762ec0a43c5d69936560
Anıl Düz <96021036+Anilduz@users.noreply.github.com>
2022-07-28 02:31:03 +0300

    karakter-ters-yazdirma-app
    karakter-ters-yazdirma-app

---
+++ karakter-ters-yazdirma-app/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace string_tersten_yazdirma{
+    class Program{
+        public static void Main(string[] args){
+        Console.Clear();
+        
+         while(true){
+            Console.WriteLine();
+            try
+            {   
+                Console.WriteLine("tersten yazılmasını istediğiniz ifadeyi giriniz");
+                string text = Console.ReadLine();
+                for (int i = text.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(text[i]);
+                }
+                
+            } 
+            catch (System.Exception ex)
+            {
+                
+                Console.WriteLine(ex);
+            }
+         }
+        }
+    }
+}
